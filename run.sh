ed8078d266f60944174ff0ec99ebaefa93ffdc88
Alex Woollam <alexjhwoollam@gmail.com>
2021-07-26 11:10:50 +0100

    Update run.sh

--- run.sh
+++ run.sh
@@ -4,12 +4,12 @@ set -x
 
 PROJECT_KEY="${PROJECT_KEY:-ConsoleApplication1}"
 PROJECT_NAME="${PROJECT_NAME:-ConsoleApplication1}"
-DT=$(date '+%d/%m/%Y %H:%M:%S')
+DT=$(date '+%d/%m/%Y-%H:%M:%S')
 PROJECT_VERSION="${PROJECT_VERSION:-1.0}"
 SONAR_HOST="${HOST:-http://localhost:9000}"
 SONAR_LOGIN_KEY="${LOGIN_KEY:-admin}"
 
-dotnet sonarscanner begin /k:$PROJECT_KEY /d:sonar.host.url=$HOST /v:$PROJECT_VERSION$DT
+dotnet sonarscanner begin /k:$PROJECT_KEY /d:sonar.host.url=$HOST /v:$PROJECT_VERSION-$DT
 dotnet build
 dotnet test
 dotnet sonarscanner
