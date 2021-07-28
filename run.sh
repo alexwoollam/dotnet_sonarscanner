8aaa10a0c702c293ee4c3cdcb21c9ea9b46f85ab
Alex Woollam <alexjhwoollam@gmail.com>
2021-07-28 14:19:18 +0100

    Update run.sh

--- run.sh
+++ run.sh
@@ -9,7 +9,7 @@ PROJECT_VERSION="${PROJECT_VERSION:-1.0}"
 SONAR_HOST="${HOST:-http://localhost:9000}"
 SONAR_LOGIN_KEY="${LOGIN_KEY:-admin}"
 
-dotnet sonarscanner begin /k:$PROJECT_KEY /d:sonar.host.url=$HOST /v:$PROJECT_VERSION-$DT
+dotnet sonarscanner begin /k:$PROJECT_KEY /d:sonar.host.url=$HOST /v:$PROJECT_VERSION-$DT /d:sonar.cs.opencover.reportsPaths="coverage.opencover.xml" /d:sonar.coverage.exclusions="**Tests*.cs"
 dotnet build
 dotnet test /p:CollectCoverage=true /p:CoverletOutputFormat=opencover
 dotnet sonarscanner
