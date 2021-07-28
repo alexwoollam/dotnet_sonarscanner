46c845ac886598f298b85666c49e5735eea45903
Alex Woollam <alexjhwoollam@gmail.com>
2021-07-28 14:21:54 +0100

    Update run.sh

--- run.sh
+++ run.sh
@@ -5,6 +5,7 @@ set -x
 PROJECT_KEY="${PROJECT_KEY:-ConsoleApplication1}"
 PROJECT_NAME="${PROJECT_NAME:-ConsoleApplication1}"
 DT=$(date '+%d/%m/%Y-%H:%M:%S')
+COVERAGE_PATH="${COVERAGE_PATH}"
 PROJECT_VERSION="${PROJECT_VERSION:-1.0}"
 SONAR_HOST="${HOST:-http://localhost:9000}"
 SONAR_LOGIN_KEY="${LOGIN_KEY:-admin}"
